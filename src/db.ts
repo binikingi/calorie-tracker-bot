a6be0141e64649f3dd4b573fe191407fd3924539
Bini Barazany <bgt636@gmail.com>
2024-11-24 23:26:07 +0200

    No ssl on connection to db

--- src/db.ts
+++ src/db.ts
@@ -5,10 +5,10 @@ import pg from "pg";
 
 export const client = new pg.Client({
   connectionString: appConfig.DATABASE_URL,
-  ssl:
-    appConfig.NODE_ENV === "production"
-      ? {
-          rejectUnauthorized: false,
-        }
-      : false,
+  //   ssl:
+  //     appConfig.NODE_ENV === "production"
+  //       ? {
+  //           rejectUnauthorized: false,
+  //         }
+  //       : false,
 });
