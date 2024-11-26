7981fe1ee5ed4eb0ea44ba634e132fdfe318bdb8
Bini Barazany <bgt636@gmail.com>
2024-11-26 15:18:48 +0200

    Remove ssl from db

--- src/db.ts
+++ src/db.ts
@@ -3,10 +3,4 @@ import pg from "pg";
 
 export const client = new pg.Client({
   connectionString: appConfig.DATABASE_URL,
-  ssl:
-    appConfig.NODE_ENV === "production"
-      ? {
-          rejectUnauthorized: false,
-        }
-      : undefined,
 });
