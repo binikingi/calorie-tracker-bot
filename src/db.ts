03cdd034feeead2f90063b7555dc64508ecd3246
Bini Barazany <bgt636@gmail.com>
2024-11-26 13:32:35 +0200

    Fix ssl

--- src/db.ts
+++ src/db.ts
@@ -3,5 +3,10 @@ import pg from "pg";
 
 export const client = new pg.Client({
   connectionString: appConfig.DATABASE_URL,
-  ssl: appConfig.NODE_ENV === "production" ? true : undefined,
+  ssl:
+    appConfig.NODE_ENV === "production"
+      ? {
+          rejectUnauthorized: false,
+        }
+      : undefined,
 });
